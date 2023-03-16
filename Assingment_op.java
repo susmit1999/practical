db45af356c3dca106fe2f3b0320d2c345344deb8
susmit1999 <127473094+susmit1999@users.noreply.github.com>
2023-03-16 12:01:20 +0530

    Add files via upload

---
+++ Assingment_op.java
@@ -0,0 +1,11 @@
+class Assingment_op{
+public static void main(String args[]){
+
+int a=10;
+
+System.out.println(a+=3);
+System.out.println(a-=5);
+System.out.println(a*=2);
+System.out.println(a/=5);
+}
+}
---
+++ logical_op.java
@@ -0,0 +1,13 @@
+class logical_op{
+public static void main(String args[]){
+
+int a=10;
+int b=5;
+int c=20;
+
+System.out.println(a>b && a<c);
+System.out.println(a==b || a<c);
+System.out.println(a>c && b<a);
+System.out.println(a<c || b>a);
+}
+}
